544f508696985e66245be7f9d3abf78a18d74a1b
quarterblue <quarterslate@gmail.com>
2021-05-05 23:04:45 -0700

    Add styledgame and styledmatrix

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import TestCell from './components/TestCell';
 
 function App() {
   return (
     <div className="App">
       <div>Tetris App</div>
+      <TestCell></TestCell>
     </div>
   );
 }
