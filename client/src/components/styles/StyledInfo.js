3867c52e6aead876ce013ea3d05ea4d6b0cca6f3
quarterblue <quarterslate@gmail.com>
2021-05-05 23:35:03 -0700

    Change styledInfo

--- client/src/components/styles/StyledInfo.js
+++ client/src/components/styles/StyledInfo.js
@@ -4,4 +4,14 @@ export const StyledInfo = styled.div`
   box-sizing: border-box;
   display: flex;
   align-items: center;
+  margin: 0 0 20px 0;
+  padding: 20px;
+  border: 4px solid #333;
+  min-height: 30px;
+  width: 100%;
+  border-radius: 20px;
+  color: ${(props) => (props.gameOver ? 'red' : '#999')};
+  background: #000;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 0.8rem;
 `;
