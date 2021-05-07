685caf5e4736ea5d57b59434b50596374334433f
quarterblue <quarterslate@gmail.com>
2021-05-06 17:07:46 -0700

    Add custom hooks

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import Start from './Start';
 import Matrix from './Matrix';
 import { createMatrix } from '../Structure';
 import { StyledGameComponent, StyledGame } from './styles/StyledGame';
 
+// Custom hooks
+import { usePlayer } from '../hooks/usePlayer';
+import { useMatrix } from '../hooks/useMatrix';
+
 const Game = () => {
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  // Player Object
+  const [player] = usePlayer();
+  const [matrix, setMatrix] = useMatrix(player);
+
   return (
     <StyledGameComponent>
       <StyledGame>
         <aside>
-          <div>
-            <Display text="HOLD" />
-            <Display text="SCORE" />
-            <Display text="ROWS" />
-            <Display text="LEVEL" />
-          </div>
+          {gameOver ? (
+            <Display gmaeOver={gameOver} text="Game Over" />
+          ) : (
+            <div>
+              <Display text="HOLD" />
+              <Display text="SCORE" />
+              <Display text="ROWS" />
+              <Display text="LEVEL" />
+            </div>
+          )}
           <Start />
         </aside>
-        <Matrix matrix={createMatrix()} />
+        <Matrix matrix={matrix} />
       </StyledGame>
     </StyledGameComponent>
   );
